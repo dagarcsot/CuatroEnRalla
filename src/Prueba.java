c1703cc74999371d0238a5352ee5209e58a73200
Ifersa <ivanfernandezarnedo@gmail.com>
2024-11-12 18:31:09 +0100

    Inicio

---
+++ src/Prueba.java
@@ -0,0 +1,8 @@
+package src;
+
+public class Prueba {
+    public static void main(String[] args) {
+        Tablero t = new Tablero();
+        t.imprimirTablero();
+    }
+}
--- src/Tablero.java
+++ src/Tablero.java
@@ -1,10 +1,28 @@
 package src;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class Tablero {
 
-    private int[][] tablero;
+    private int[][] tablero; //tiene 7 filas y 6 columnas
+    private static final int FILAS = 7;
+    private static final int COLUMNAS = 6;
+
+    //Post: crea el tablero vacio
+    public Tablero(){
+        this.tablero = new int[FILAS][COLUMNAS];
+        for(int i=0;i<FILAS;i++){
+            for(int j=0;j<COLUMNAS;j++){
+                this.tablero[i][j] = 0;
+            }
+        }
+    }
+
+    public void imprimirTablero() {
+        for(int i=0;i<FILAS;i++){
+            for(int j=0;j<COLUMNAS;j++){
+                System.out.println(tablero[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
 
 }
