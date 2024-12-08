9d3440c0e4d1a5f7d9dadfe344fc5f6d0cc5b795
Ifersa <ivanfernandezarnedo@gmail.com>
2024-12-08 19:29:59 +0100

    Cambios GestionCliente

--- src/GestionCliente.java
+++ src/GestionCliente.java
@@ -4,11 +4,16 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class GestionCliente implements Runnable {
 
     private static Socket jugadorEnEspera; // Variable estática para almacenar al jugador en espera
     private Socket cliente;
+    private static ExecutorService pool = Executors.newCachedThreadPool(); // Pool de hilos para manejar las partidas
 
     public GestionCliente(Socket cliente) {
         this.cliente = cliente;
@@ -56,13 +61,16 @@ public class GestionCliente implements Runnable {
                             } else {
                                 // Si hay jugador en espera, se inicia una partida
                                 Socket jugador1 = jugadorEnEspera;
-                                jugadorEnEspera = null;
+                                jugadorEnEspera = null; // El jugador en espera ya está emparejado
 
-                                new Thread(new Partida(jugador1, cliente)).start();
                                 oos.writeObject("Jugador encontrado. ¡Comienza la partida!");
                                 oos.flush();
+
+                                // Usamos el pool de hilos para gestionar la partida
+                                pool.execute(new Partida(jugador1, cliente));
                             }
                         }
+
                         break;
                     }
                     default: {
@@ -75,7 +83,7 @@ public class GestionCliente implements Runnable {
         } catch (IOException | ClassNotFoundException e) {
             e.printStackTrace();
         } finally {
-            // Cerrar conexión si no está cerrada
+            // Cerrar la conexión si no está cerrada
             try {
                 if (!cliente.isClosed()) {
                     cliente.close();
